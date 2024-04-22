603d6bc0fd0af4857908437c7539128be38d7929
Kamil <KamilWodowski@o2.pl>
2024-04-22 08:45:59 +0200

    setup components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {HomeComponent} from "./components/home/home.component";
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '/login', component: LoginComponent},
+  { path: '/register', component: RegisterComponent},
+  { path: '/app', component: HomeComponent, children: [
+
+    ]},
+  { path: '**', component: PageNotFoundComponent}
+];
